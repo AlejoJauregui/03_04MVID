6d5cd9feb4b842ee0afd3cafb99fadb1fa20460f
Alejo_Jauregui <alejojs9409@gmail.com>
2020-01-25 12:41:20 -0500

    Third Commit: Painting triangles with different colors and a hexagon
    We painted two triangles with two different programs to difference on of each other. Finally, we painted a hexagon with 8 triangles

--- projects/EJ02_02/main.cpp
+++ projects/EJ02_02/main.cpp
@@ -72,15 +72,11 @@ uint32_t createVertexData(uint32_t* VBO, float* vertices, uint32_t size)
     uint32_t VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, VBO);
-    //glGenBuffers(1, EBO);
 
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, *VBO);
     glBufferData(GL_ARRAY_BUFFER, size* sizeof(float), vertices, GL_STATIC_DRAW);
-
-    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, *EBO);
-    //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indexes), Indexes, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void*)0);
     glEnableVertexAttribArray(0);
